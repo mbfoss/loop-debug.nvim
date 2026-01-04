fe193ae57c9c2317835698efd3101c6d6d2a368d
mbfoss <mbfoss@fastmail.com>
2026-01-04 16:33:35 +0100

    update

--- lua/loop-debug/provider.lua
+++ lua/loop-debug/provider.lua
@@ -1,6 +1,7 @@
 local run = require('loop-debug.run')
 local ui = require('loop-debug.ui')
 local persistence = require('loop-debug.persistence')
+local jsontools = require('loop.tools.json')
 
 ---@type loop.TaskProvider
 local task_provider =
@@ -25,6 +26,13 @@ local task_provider =
     get_task_templates = function(_)
         local templates = require('loop-debug.templates')
         return templates
+    end,
+    get_task_preview = function (task)
+        local cpy = vim.fn.copy(task)
+        local templates = require('loop-debug.templates')
+        ---@diagnostic disable-next-line: undefined-field, inject-field
+        cpy.__order = templates[1].task.__order
+        return jsontools.to_string(cpy), "json"        
     end,
     start_one_task = run.start_debug_task,
 }
